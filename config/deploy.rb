9f76e9008f2d0b04e04e73714ffbe10a37056705
Stephen Karger <stephen.karger@gmail.com>
2015-04-16 22:15:21 -0400

    deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.4.0'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:skarger/torquebox-sidekiq-demo.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/deployments/my_app_name'
 
 # Default value for :scm is :git
 # set :scm, :git
